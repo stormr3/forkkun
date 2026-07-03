b411b9894fd3a4cddb8c20efca719901eb2360db
stormr3 <stormr333@gmail.com>
2026-07-04 03:37:44 +0530

    Initial package with RViz and Gazebo

---
+++ src/forkkun_description/launch/gazebo_harmonic.launch.py
@@ -0,0 +1,55 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from launch.substitutions import PathJoinSubstitution
+
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    share_dir = get_package_share_directory("forkkun_description")
+    xacro_file = os.path.join(share_dir,"urdf","Forkkun.xacro")
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_urdf = robot_description_config.toxml()
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        parameters=[{"robot_description": robot_urdf}]
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare("ros_gz_sim"),
+                "launch",
+                "gz_sim.launch.py"
+            ])
+        ),
+        launch_arguments={
+            "gz_args": "-r empty.sdf"
+        }.items()
+    )
+
+    spawn_robot = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-topic", "robot_description",
+            "-name", "Forkkun"
+        ],
+        output="screen"
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        gazebo,
+        spawn_robot,
+    ])
